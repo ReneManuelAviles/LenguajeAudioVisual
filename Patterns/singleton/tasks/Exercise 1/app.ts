8805001e83b0cd582bce6dcefd204bd91a85bb7e
Rene Aviles <renemanuelaviles@gmail.com>
2023-09-23 08:18:41 -0400

    prac3

--- Patterns/singleton/tasks/Exercise 1/app.ts
+++
@@ -1,7 +0,0 @@
-import { LoggerService } from './loggerService';
-
-const logger1 = LoggerService.getInstance("log.txt");
-const logger2 = LoggerService.getInstance("log.txt");
-
-logger1.logMessage("This is a message from logger1");
-logger2.logMessage("This is a message from logger2");
